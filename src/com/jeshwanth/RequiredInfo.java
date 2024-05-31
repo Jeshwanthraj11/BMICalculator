6b93c12a29eea9069fbfc4e0da7dba3f1b8a7031
Jeshwanthraj11 <157581363+Jeshwanthraj11@users.noreply.github.com>
2024-06-01 01:39:23 +0530

    Added some features to calculate BMI

--- src/com/jeshwanth/RequiredInfo.java
+++ src/com/jeshwanth/RequiredInfo.java
@@ -1,11 +1,27 @@
 package com.jeshwanth;
 
 public class RequiredInfo {
-    private int weight;
-    private int height;
+    private double weight;
+    private double height;
 
-    RequiredInfo(int weight,int height){
+    RequiredInfo(double weight,double height){
         this.weight = weight;
         this.height = height;
+    }
+
+    private double BMIScore(){
+        return this.weight /this.height *this.height;
+    }
+
+     void BMI(){
+        if (BMIScore() > 24.9) {
+            System.out.println("You're BMi score is " + BMIScore() + "\n You're obese");
+        }
+         else if(BMIScore() < 18.5){
+            System.out.println("You're BMi score is "+BMIScore()+"\n You should put on some weight");
+        }
+        else{
+            System.out.println("You're BMi score is "+BMIScore()+"\n You're in healthy range");
+        }
     }
 }
