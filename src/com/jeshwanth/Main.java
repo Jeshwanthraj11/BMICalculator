f2a59b13df73080579a1f8db0906aa28256f7876
Jeshwanthraj11 <157581363+Jeshwanthraj11@users.noreply.github.com>
2024-06-01 01:40:50 +0530

    Basic BMI calculator

--- src/com/jeshwanth/Main.java
+++ src/com/jeshwanth/Main.java
@@ -1,7 +1,18 @@
 package com.jeshwanth;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter weight in kilograms");
+        double weight = sc.nextDouble();
+        System.out.println("Enter height in meters");
+        double height = sc.nextDouble();
+
+        RequiredInfo info = new RequiredInfo(weight,height);
+        info.BMI();
+
 
     }
 }
