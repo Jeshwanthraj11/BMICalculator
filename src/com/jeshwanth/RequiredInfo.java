6883cc1e834fbe17c311c2b268913eacab12b675
Jeshwanthraj11 <157581363+Jeshwanthraj11@users.noreply.github.com>
2024-06-01 01:08:05 +0530

    Added src file

---
+++ src/com/jeshwanth/RequiredInfo.java
@@ -0,0 +1,11 @@
+package com.jeshwanth;
+
+public class RequiredInfo {
+    private int weight;
+    private int height;
+
+    RequiredInfo(int weight,int height){
+        this.weight = weight;
+        this.height = height;
+    }
+}
